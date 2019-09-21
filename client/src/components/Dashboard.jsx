5b2445d29336df7691f07e633ebdaba5e9026f82
afindl3 <alisonfindlay93@hotmail.com>
2019-09-21 18:41:31 -0400

    Refactor to have needed seperate from categories

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { Form, Button, InputGroup, FormControl, Modal } from 'react-bootstrap';
+import { Button, Modal, Container, Row, Col } from 'react-bootstrap';
 import styled from 'styled-components';
 import axios from 'axios';
-import wifi from '../assets/wifi.jpeg';
+import * as Constants from "../utils/constants";
 
 class Dashboard extends Component {
   constructor(){
@@ -13,7 +13,8 @@ class Dashboard extends Component {
       recipientCode: '',
       amount: '',
       category: '',
-      showModal: false
+      showModal: false,
+      isDisabled: true
     }
     this.handleChange = this.handleChange.bind(this);
     this.connectWithRecipient = this.connectWithRecipient.bind(this);
@@ -36,13 +37,7 @@ class Dashboard extends Component {
 
     this.setState({
       showModal: true,
-      inNeedOf: ['Food', 'Clothing']
-    });
-  }
-
-  transferFunds() {
-    axios.post('').then(res => {
-
+      inNeedOf: ['Food', 'Clothing', 'Any']
     });
   }
 
@@ -52,44 +47,92 @@ class Dashboard extends Component {
     })
   }
 
+  setCategory(category) {
+    console.log(category)
+    this.setState({ category: category });
+  }
+
+  setAmount(amount) {
+    console.log(amount)
+    this.setState({ 
+      amount: amount,
+      isDisabled: false
+    });
+  }
+
+  transferFunds() {
+    axios.post('').then(res => {
+
+    });
+    this.setState({
+      showModal: false
+    })
+  }
+
   render() {
     return (
       <DashboardContainer>
-        <img className="center" src={wifi}/>
+        <img className="center" src={Constants.Images.wifi} width="30px" alt=""/>
         <p>Find someone in need near you...</p>
         <Button className="center" variant="primary" onClick={this.connectWithRecipient}>Search</Button>
 
-        <Modal show={this.state.showModal} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Success!</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p>You just connected with Joe!</p>
+        <ModalContainer show={this.state.showModal} onHide={this.handleClose}>
+          <img classname="backdrop-img" src={Constants.Images.backdrop}/>
+          <div className="backdrop-color">
+            <Modal.Header closeButton></Modal.Header>
+            
+            <Modal.Body>
+              <img className="center" src={Constants.Images.joe} width="82px" alt=""/>
+              <h1>Joe</h1>
+              <br></br><br></br>
 
-            <Form>
-              <Form.Group controlId="exampleForm.ControlSelect1">
-                <Form.Label>Joe is most in need of:</Form.Label>
-                <Form.Control as="select" name="category" onChange={this.handleChange}>
-                  <option>Food</option>
-                  <option>Clothing</option>
-                  {/* { this.state.inNeedOf.map((category, i) => (
-                    <option key={i}>{{category}}</option>
-                  ))} */}
-                </Form.Control>
-              </Form.Group>
+              <h2>I need Most</h2>
+              <Row>
+                { this.state.inNeedOf.map((needed, i) => (
+                  <Col key={i} onClick={() => this.setCategory(needed)}>
+                    <div className="bubble-needed"></div>
+                    <span>{needed}</span>
+                  </Col>
+                ))}
+              </Row>
+              <br></br><br></br>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Monetary value:</Form.Label>
-                <Form.Control type="text" placeholder="Enter donation amount" name="amount" onChange={this.handleChange}/>
-              </Form.Group>
-            </Form>
+              <h2>Give Joe a donation</h2>
+              <Row>
+                { Constants.Categories.map((category, i) => (
+                  <Col key={i}>
+                    <img className="bubble-categories center" width="61px" alt=""
+                         src={category.img} onClick={() => this.setCategory(category.name)}/>
+                    <p>{category.name}</p>
+                  </Col>
+                ))}
+              </Row>
+              <br></br><br></br>
 
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>Close</Button>
-            <Button variant="primary" onClick={this.transferFunds}>Donate Now</Button>
-          </Modal.Footer>
-        </Modal>
+              { this.state.category &&
+                <div>
+                  <h2>Amount</h2>
+                  <Row>
+                  { Constants.MonetaryValues.map((amount, i) => (
+                    <Col key={i}>
+                      <div className="bubble-amount center" width="61px" 
+                           onClick={() => this.setAmount(amount)}>
+                        <p className="text-amount">${amount}</p>
+                      </div>
+                    </Col>
+                    
+                  ))}
+                  </Row>
+                </div>
+              }
+            </Modal.Body>
+
+            <Modal.Footer>
+              <button className="button-donate center" variant="primary" 
+                      disabled={this.state.isDisabled} onClick={this.transferFunds}>Donate Now</button>
+            </Modal.Footer>
+          </div>
+        </ModalContainer>
       </DashboardContainer>
     );
   }
@@ -105,10 +148,81 @@ const DashboardContainer = styled.div`
     display: block;
     margin: 0 auto;
   }
-  img {
-    width: 30px;
-  }
   p {
     text-align: center;
+  }
+`
+
+const ModalContainer = styled(Modal)`
+  .backdrop-img {
+    position: absolute;
+    width: 500px;
+    top: -10px;
+  }
+  .backdrop-color {
+    background-color: #E1E8F2;
+  }
+  
+  .center {
+    display: block;
+    margin: 0 auto;
+  }
+  h1 {
+    text-align: center;
+    font-size: 18px;
+    font-weight: bold;
+    color: #42404B;
+    margin: 10px 0 0 0;
+  }
+  h2 {
+    text-align: center;
+    font-size: 18px;
+    color: #42404B; 
+  } 
+  p, span {
+    text-align: center;
+    font-size: 14px;
+    color: #615F6C;
+  }
+
+  .bubble-needed {
+    border-radius: 50%;
+    background-color: #91C0FF;
+    cursor: pointer;
+    height: 8px;
+    width: 8px;
+
+    display: inline-block;
+    margin-right: 5px;
+  }
+  .bubble-categories {
+    border-radius: 50%;
+    background-color: #91C0FF;
+    cursor: pointer;
+    height: 61px;
+    width: 61px;
+  }
+  .bubble-amount {
+    border-radius: 50%;
+    background-color: #91C0FF;
+    cursor: pointer;
+    height: 61px;
+    width: 61px;
+  }
+  .text-amount{
+    padding-top: 17px;
+  }
+
+  .button-donate {
+    background-color: #F16522;
+    width: 150px;
+    height: 46px;
+    color: #FFFFFF;
+    border: none;
+    border-radius: 23px;
+    cursor: pointer;
+    :disabled {
+      opacity: 0.5;
+    }
   }
 `
